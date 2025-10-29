06375b1145819fca827b47c82343a4e80840a136
Cauã Andrade Vieira <20241STADS002@cefetes.br>
2025-10-29 12:59:09 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddressComponent } from './components/address/address.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "address",
+        component: AddressComponent
+    }
+];
