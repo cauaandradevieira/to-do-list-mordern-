f454136dde225d2f88f5490c177cc6a1d537fd5a
Cauã Andrade Vieira <20241STADS002@cefetes.br>
2025-10-31 10:10:12 -0300

    add test for FormControl and service integration

--- src/app/components/address/address.component.ts
+++ src/app/components/address/address.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AddressService } from '../../services/address.service';
+import { Address } from '../../interface/address';
 
 @Component({
   selector: 'app-address',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './address.component.html',
   styleUrls: ['./address.component.css']
 })
-export class AddressComponent {
+export class AddressComponent implements OnInit {
 
+  form!: FormGroup;
+  addressService = inject(AddressService);
+  address!: Address;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      cep: ['', [Validators.required, Validators.maxLength(9)]]
+    }
+    );
+  };
+
+  clickTest() {
+    if (this.form.invalid) {
+      console.log("Form invalid" + this.form.errors);
+      return;
+    }
+
+    this.addressService.get(this.form.value['cep']).subscribe(
+      {
+        next: (data) => { this.address = data }
+      }
+    );
+  }
 }
