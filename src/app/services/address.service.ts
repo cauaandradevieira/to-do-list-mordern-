f269cbe52ba5425e86623a9aca9756d63f597da7
Cauã Andrade Vieira <20241STADS002@cefetes.br>
2025-10-31 09:54:30 -0300

    implement integration with the ViaCEP API

--- src/app/services/address.service.ts
+++ src/app/services/address.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Address } from '../interface/address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
 
-  constructor() { }
+  api: string = "https://viacep.com.br/ws/"
+
+  constructor(private http:HttpClient) { }
+
+  get(cep: string) : Observable<Address>
+  {
+    const cepFormatted: string = cep.replace("-", "");
+    const url: string = `${this.api}/${cepFormatted}/json/`;
+    return this.http.get<Address>(url);
+  }
 }
